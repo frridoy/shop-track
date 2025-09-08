d99737f7caf134fe237f8f184d2ca115d59216ff
frridoy <foyzur.r.h@gmail.com>
2025-09-08 18:40:27 +0600

    updated

--- resources/views/admin/order/create.blade.php
+++ resources/views/admin/order/create.blade.php
@@ -48,8 +48,11 @@
 
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
+    <script src="https://code.jquery.com/jquery-3.7.0.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
     <link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" />
     <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
+
 
     <script>
         $(document).ready(function() {
